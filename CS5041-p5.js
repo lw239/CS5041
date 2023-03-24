90a49b9251a08cdddb719ed78463c41079663cab
Lauren Wells <lw239@st-andrews.ac.uk>
2023-03-24 09:16:35 +0000

    Forgot to commit this stuff

--- CS5041-p5.js
+++ CS5041-p5.js
@@ -1,17 +1,29 @@
-var textPixels = 22;
+//var barLines = ??
+//Note to Lauren: Bars have 5 lines
+
 
 function setup() {
-    createCanvas(300, 300);
+    createCanvas(660, 360);
+    // loadImage('bars.png', img => {
+    //     image(img, 0, 0);
 }
 
+
+// I don't know if I have to keep everyting in here but it looks like 
+// you can only draw with this one function 
 function draw() {
-    background(220, 20, 20)
-    textSize(textPixels)
-    text("This is example 1: simple", 30, 30)
-    text("simple script, in a page", 35, 60)
-    rect(width / 3, height / 3, 100, 100)
+    line(40, 50, 500, 50);
+    line(40, 100, 500, 100);
+    line(40, 150, 500, 150);
+    line(40, 200, 500, 200);
+    line(40, 250, 500, 250);
 }
+
+// //I guess "draw" is the only function name that can be used...
+// function draw(){
+
+// }
 
 function keyPressed() {
-    textPixels -= 1;
+
 }
